3395d2e09d1db0f4c9307dc309e145703ff1ddb5
Maxim Mitin <maximus405@gmail.com>
2022-04-03 10:53:31 +0300

    add blog model, created new view, work on logic and presentation

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Blog
 # Create your views here.
+def all_blogs(request):
+    blogs = Blog.objects.order_by('-dates') #order by date [:5] show first 5
+    return render(request, 'blog/all_blogs.html', {'blogs':blogs})
