08aa8738e495cc312b9f4df77e060dab826736ed
Maxim Mitin <maximus405@gmail.com>
2022-04-03 14:39:34 +0300

    work on appearance , date format , description format , add links from home page to details and all blogs

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=100)
     dates = models.DateField()
     description = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.title
